f4787ac9f263e250ba03522c3a55ccf6d14810f8
WalterPardini <walterpardini@outlook.com>
2022-04-27 14:17:29 -0300

    Usuarios mejorados

--- db.php
+++
@@ -1,4 +0,0 @@
-<?php
-$conexion=mysqli_connect("localhost","root","","agraria");
-?>
-<H1></H1>
--- validar.php
+++ validar.php
@@ -1,30 +1,82 @@
 <?php
-include('db.php');
-$usuario=$_POST['usuario'];
-$contraseña=$_POST['contraseña'];
-session_start();
-$_SESSION['usuario']=$usuario;
+    include_once 'database.php';
+    
+    session_start();
 
+    if(isset($_GET['cerrar_sesion'])){
+        session_unset(); 
 
-$conexion=mysqli_connect("localhost","root","","login");
+        // destroy the session 
+        session_destroy(); 
+    }
+    
+    if(isset($_SESSION['rol'])){
+        switch($_SESSION['rol']){
+            case 1:
+                header('location: admin.php');
+            break;
 
-$consulta="SELECT*FROM usuarios where usuario='$usuario' and contraseña='$contraseña'";
-$resultado=mysqli_query($conexion,$consulta);
+            case 2:
+            header('location: colab.php');
+            break;
 
-$filas=mysqli_num_rows($resultado);
+            default:
+        }
+    }
 
-if($filas){
-  
-    header("location:admin.html");
+    if(isset($_POST['username']) && isset($_POST['password'])){
+        $username = $_POST['username'];
+        $password = $_POST['password'];
 
-}else{
-    ?>
-    <?php
-    include("ingreso.html");
+        $db = new Database();
+        $query = $db->connect()->prepare('SELECT *FROM usuarios WHERE username = :username AND password = :password');
+        $query->execute(['username' => $username, 'password' => $password]);
 
-    ?>
-    <h1 class="bad">ERROR DE AUTENTIFICACION</h1>
-    <?php
+        $row = $query->fetch(PDO::FETCH_NUM);
+        
+        if($row == true){
+            $rol = $row[3];
+            
+            $_SESSION['rol'] = $rol;
+            switch($rol){
+                case 1:
+                    header('location: admin.php');
+                break;
 
-}
-mysqli_free_result($resultado);
+                case 2:
+                header('location: colab.php');
+                break;
+
+                default:
+            }
+        }else{
+            // no existe el usuario
+            echo "Nombre de usuario o contraseña incorrecto";
+        }
+        
+
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Conectar</title>
+    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="css/cabecera.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css">
+</head>
+<body>
+   <form action="validar.php" method="post">
+   <h1 class="animate__animated animate__backInLeft">Ingrese usuario y contraseña</h1>
+   <p>Usuario <input type="text" placeholder="ingrese su nombre" name="username"></p>
+   <p>Contraseña <input type="password" placeholder="ingrese su contraseña" name="password"></p>
+   <input type="submit" value="Ingresar">
+   
+
+</body>
+</html>
